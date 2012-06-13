df6d762a42b2ef30f70e108cbaaba0e876a46490
tcsavage <tcsavage@gmail.com>
2012-06-13 22:58:22 +0100

    Added config model and default error handler for mapping to exceptions.

--- index.php
+++ index.php
@@ -4,6 +4,36 @@
 
 define("ROOT",str_replace("\\","/",__DIR__));
 
+// Handle errors as exceptions.
+function exception_error_handler($errno, $errstr, $errfile, $errline )
+{
+	$exit = false;
+	switch ( $errno ) {
+		case E_USER_ERROR:
+			$type = 'Fatal Error';
+			$exit = true;
+			break;
+		case E_USER_WARNING:
+		case E_WARNING:
+			$type = 'Warning';
+			break;
+		case E_USER_NOTICE:
+		case E_NOTICE:
+		case @E_STRICT:
+			$type = 'Notice';
+			break;
+		case @E_RECOVERABLE_ERROR:
+			$type = 'Catchable';
+			break;
+		default:
+			$type = 'Unknown Error';
+			$exit = true;
+			break;
+	}
+    if ($exit) throw new ErrorException($errstr, 0, $errno, $errfile, $errline);
+}
+set_error_handler("exception_error_handler");
+
 require_once("config.php");
 require_once("system/bootstrap.php");
 
