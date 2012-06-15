2691f3c502757474d853eab61f0b80f734aaefd0
tcsavage <tcsavage@gmail.com>
2012-06-15 20:18:32 +0100

    Started cli tool.

--- cli.php
+++ cli.php
@@ -1,6 +1,32 @@
 <?php
 
-echo("Pangolin Framework CLI\n");
-$line = trim(fgets(STDIN));
-echo("You said '" . $line . "'."
+define("ROOT",str_replace("\\","/",__DIR__));
+
+require_once("config.php");
+require_once("system/bootstrap.php");
+
+$db = new \pangolin\Database($dbconfig["development"]);
+$db->connect();
+
+foreach ($installedapps as $app)
+{
+	require_once("apps/".$app."/__init.php");
+}
+
+switch ($argv[1])
+{
+	case "makedb":
+		foreach (\admin\Site::getModels() as $model)
+		{
+			echo("Building " . $model['fullpath'] . "\n");
+			$makequery = $model['fullpath']::buildSQLCreate();
+			$makequery->run();
+		}
+	break;
+
+	default:
+		echo("Unknown command.\n");
+	break;
+}
+
 exit(0);
