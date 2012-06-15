322dfde697965d15348f5ccf46315f31d643af00
tcsavage <tcsavage@gmail.com>
2012-06-15 20:13:12 +0100

    Documented and removed foreign key attributes.

--- system/fields/Field.php
+++ system/fields/Field.php
@@ -1,23 +1,42 @@
 <?php namespace pangolin;
 
+/*
+Field base class.
+
+Defined the basis for all types of which models are comprised.
+Fields are essentially wrappers around values that provide extra information for: generating SQL statements, building HTML displays and forms, and validating input.
+*/
 abstract class Field
 {
+	// Defines the order in which fields are displayed in tables and forms
 	public $order = -1;
+
+	// True if field is prirary key (probably only used for automatic id field).
 	public $primarykey = False;
-	public $foreignkey = null;
+
+	// True if value can be null.
 	public $nullable = True;
+
+	// True if field auto-increments (probably only used for automatic id field and when using only one database server).
 	public $autoincrement = False;
 	
+	// The name of the field's variable. Used to name HTML elements, SQL columns etc.
 	public $name = null;
+
+	// Human readable name used when rendering tables, form controls, etc.
 	public $prettyname = null;
+
+	// Human readable text displayed next to form elements to guide users.
 	public $helptext = null;
+
+	// The actual value of the field.
 	private $value = null;
 	
+	// Default constructor. Sets properties from provided array.
 	public function __construct($options, $value)
 	{
 		if (isset($options["order"])) $this->order = $options["order"];
 		if (isset($options["primarykey"])) $this->primarykey = $options["primarykey"];
-		if (isset($options["foreignkey"])) $this->foreign = $options["foreignkey"];
 		if (isset($options["nullable"])) $this->nullable = $options["nullable"];
 		if (isset($options["autoincrement"])) $this->autoincrement = $options["autoincrement"];
 		if (isset($options["prettyname"])) $this->prettyname = $options["prettyname"];
@@ -26,26 +45,31 @@ abstract class Field
 		$this->value = $value;
 	}
 	
+	// Returns value.
 	public function getValue()
 	{
 		return $this->value;
 	}
 	
+	// Sets value.
 	public function setValue($value)
 	{
 		$this->value = $value;
 	}
 
+	// Renders HTML form control.
 	public function renderInput($attributes = null)
 	{
 		return "";
 	}
 
+	// Returns type for SQL representation.
 	public function SQLType()
 	{
 		return "";
 	}
 
+	// Generates full SQL definition for defining a column.
 	public function renderSQLDef()
 	{
 		return "";
--- system/fields/TextField.php
+++ system/fields/TextField.php
@@ -1,15 +1,27 @@
 <?php namespace pangolin;
 
+/*
+Test field class.
+
+Defines a text field that is either a TEXT or VARCHAR depending on max length.
+*/
 class TextField extends Field
 {
+	// Maximum length of text.
 	private $maxlength = 200;
 	
+	// Default constructor.
 	public function __construct($options, $value)
 	{
+		// Set base attributes.
 		parent::__construct($options, $value);
+
+		// Set extended attributes.
 		if (isset($options["maxlength"])) $this->maxlength = $options["maxlength"];
 	}
 	
+	// Validate input before setting it.
+	// TODO: move validation out into seperate function.
 	public function setValue($value)
 	{
 		if (strlen($value) > $this->maxlength)
@@ -19,6 +31,8 @@ class TextField extends Field
 		parent::setValue($value);
 	}
 
+	// Renders HTML form control.
+	// TODO: render textarea for fields over a certain length.
 	public function renderInput($attributes = null)
 	{
 		$out = '<input type="text" id="'.$this->name.'" name="'.$this->name.'"';
@@ -35,11 +49,14 @@ class TextField extends Field
 		return $out;
 	}
 
+	// Returns type for SQL representation.
+	// TODO: use TEXT, LONGTEXT for very high max length or null.
 	public function SQLType()
 	{
 		return "VARCHAR";
 	}
 
+	// Generates full SQL definition for defining a column.
 	public function renderSQLDef()
 	{
 		$elems = array();
