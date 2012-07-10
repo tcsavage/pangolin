4d9d5ffff0f446301192cce86adbf950705ff5c2
Tom Savage <tcsavage@gmail.com>
2012-07-10 20:51:32 +0100

    Added phpdoc to HTTPRequest

--- system/HTTPRequest.php
+++ system/HTTPRequest.php
@@ -1,13 +1,30 @@
 <?php namespace pangolin;
 
+/**
+ * Represents everything you should need to know about a request.
+ * @package pangolin
+ */
 class HTTPRequest
 {
+	/** The HTTP verb of the request. */
 	private $requestMethod;
+
+	/** Array of request variables (POST, GET and PUT only). */
 	private $requestVars = array();
+
+	/** Parsed JSON data in data request var. */
 	private $data;
+
+	/** Accepted response types. */
 	private $accept;
+
+	/** True if ajax request. */
 	private $ajax;
 
+	/**
+	 * Constructor.
+	 * @return HTTPRequest
+	 */
  	public function __construct()
 	{
 		$this->requestMethod = strtolower($_SERVER['REQUEST_METHOD']);
@@ -34,31 +51,55 @@ class HTTPRequest
 		$this->ajax = $_SERVER['HTTP_X_REQUESTED_WITH'] == 'XMLHttpRequest';
 	}
 	
+	/**
+	 * Get HTTP verb.
+	 * @return string
+	 */
 	public function getMethod()
 	{
 		return $this->requestMethod;
 	}
 
+	/**
+	 * Array of request variables (POST, GET and PUT only).
+	 * @return array
+	 */
 	public function getVars()
 	{
 		return $this->requestVars;
 	}
 
+	/**
+	 * Parsed JSON data in data request var.
+	 * @return array/object
+	 */
 	public function getData()
 	{
 		return $this->data;
 	}
 
+	/**
+	 * Uploaded files.
+	 * @return array
+	 */
 	public function getFiles()
 	{
 		return $_FILES;
 	}
 
+	/**
+	 * Accepted response types.
+	 * @return array
+	 */
 	public function getHttpAccept()
 	{
 		return $this->accept;
 	}
 
+	/**
+	 * Is this an ajax request?
+	 * @return bool
+	 */
 	public function isAjax()
 	{
 		return $this->ajax;
