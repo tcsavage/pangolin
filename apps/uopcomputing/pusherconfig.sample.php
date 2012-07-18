423832e94c3e25ddd06af8c686dcb637562c7f13
Tom Savage <tcsavage@gmail.com>
2012-07-18 19:21:33 +0100

    Added config.php and pusherconfig.php to gitignore. Renamed each to be sample that devs will need to fill out. This is to make sure passwords etc stay out of the repo

---
+++ apps/uopcomputing/pusherconfig.sample.php
@@ -0,0 +1,3 @@
+<?php
+$pusherconfig = array("appid" => "", "key" => "", "secret" => "");
+\pangolin\Template::assignGlobal("pusherkey", $pusherconfig["key"]);
---
+++ config.sample.php
@@ -0,0 +1,23 @@
+<?php
+
+$dbconfig = array(
+	"development" => array(
+		"user" => "root",
+		"pass" => "rootpw",
+		"name" => "uopcomputing",
+		"engine" => "mysql",
+		"host" => "localhost",
+	),
+);
+
+$installedapps = array(
+	"admin",
+	"uopcomputing"
+);
+
+$projectname = "UoP Computing";
+
+$siteDomain = "localhost";
+
+// Warning level 0-3. 0 for silence. 3 for everything including debug messages.
+$warninglevel = 2;
